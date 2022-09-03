a612c868b04d867c1056acbaa4eaa4802a66daba
Lipsum <m.margaritapopova@gmail.com>
2022-09-03 21:43:57 +0300

    Пример для роутинга

--- index.php
+++ index.php
@@ -14,3 +14,15 @@ var_dump($task->getAvailableActions($customerId));
 
 echo md5('123');
 
+
+// var_dump($_SERVER);
+
+$url = $_SERVER['REQUEST_URI'];
+
+echo $url;
+
+if ($url === '/add-task') {
+    require 'pages/add-task.php';
+} elseif ($url === '/edit-profile') {
+    require 'pages/edit-profile.html';
+}
