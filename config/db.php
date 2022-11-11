e88deefab8800b3a8cc85bbceb2fc371d07f9e85
Lipsum <m.margaritapopova@gmail.com>
2022-11-11 11:04:09 +0300

    fix

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=taskforce',
-    'username' => 'margo',
+    'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
     'on afterOpen' => function ($event) {
